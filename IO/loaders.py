ee07ce114346f7f451cf9fb898c9edb4ec9f3950
Sihao Lu <sihao.lu@gmail.com>
2019-01-10 23:22:27 +0000

    added docstring for load_tiff() function

--- IO/loaders.py
+++ IO/loaders.py
@@ -3,6 +3,16 @@ from scipy.io import loadmat
 
 
 def load_tiff(path):
+    """
+    Loading a .tiff file using the scikit-image `io.imread()` function
+
+    Args:
+        path (str): The path of the .tiff file to load.
+
+    Returns:
+        ndarray: A dictionary with the image as a matrix under the *im* key.
+    """
+
     mat = dict()
     mat['im'] = io.imread(path)
 
